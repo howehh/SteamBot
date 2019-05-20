5288b63b2b3b960761e431561746006c645f5f22
howeh <howehh@yahoo.com>
2019-05-19 21:17:15 -0700

    Abstracted bot to not just be restricted to spamming

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const SteamUser = require('steam-user');
 const config = require('./config');
 const axios = require('axios');
+const spammer = require('./lib/spammer');
 
 const client = new SteamUser();
 
@@ -14,22 +15,8 @@ client.logOn({
 // Login message when successfully logged in
 client.on('loggedOn', function() {
    console.log("Login successful");
+   startProcess();
 });
-
-// Gets a list of image links from raw pastebin (label array as "links")
-let arrIndex = 0;
-
-function sendMessage() {
-   axios.get("https://pastebin.com/raw/" + config.pastebin).then(function(response) {
-      client.chatMessage(config.targetID, response.data.links[arrIndex]);
-      if (++arrIndex === response.data.links.length) arrIndex = 0;
-   });
-}
-
-// Interval to spam the target ID 
-setInterval(function() {
-   sendMessage();
-}, 60000);
 
 // Logs a user's message to console
 client.on("friendMessage", function(steamID, message) {
@@ -37,11 +24,29 @@ client.on("friendMessage", function(steamID, message) {
 });
 
 function getFormattedDate() {
-    let date = new Date();
-    let minutes = (date.getMinutes() < 10) ? date.getMinutes() + "0" : date.getMinutes();
-    let seconds = (date.getSeconds() < 10) ? date.getSeconds() + "0" : date.getSeconds();
-    let str = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() +
-            " " +  date.getHours() + ":" + minutes + ":" + seconds;
+   let date = new Date();
+   let minutes = (date.getMinutes() < 10) ? date.getMinutes() + "0" : date.getMinutes();
+   let seconds = (date.getSeconds() < 10) ? date.getSeconds() + "0" : date.getSeconds();
+   let str = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() +
+         " " +  date.getHours() + ":" + minutes + ":" + seconds;
 
-    return str;
+   return str;
+}
+
+// Run a module based on command line args
+function startProcess() {
+   switch(process.argv[2]) {
+      case "spam":
+         const userID = process.argv[3];
+         const pastebinID = process.argv[4];
+         const seconds = process.argv[5];
+         if (userID !== undefined && pastebinID !== undefined && seconds !== undefined) {
+            spammer.startSpam(client, userID, pastebinID, seconds);
+         } else {
+            console.log("Usage: node index.js spam [steamID] [pastebin ID] [spam interval]");
+         }
+         break;
+      default:
+         console.log("Usage: node index.js [arg 1] [arg 2] ... [arg n]\n");
+   }
 }
