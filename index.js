304cdf93a42d7595f82375ad48b4cdbb436ce8e4
Howie N <howien@uw.edu>
2019-09-22 23:40:43 -0700

    Refactored handling args to more appropriate places

--- index.js
+++ index.js
@@ -13,24 +13,10 @@ bot.client.on('loggedOn', function() {
 function startProcess() {
    switch(process.argv[2]) {
       case "spam":
-         const userID = process.argv[3];
-         const pastebinID = process.argv[4];
-         const seconds = process.argv[5];
-         if (userID !== undefined && pastebinID !== undefined && seconds !== undefined) {
-            spammer.startSpam(userID, pastebinID, seconds);
-         } else {
-            console.log("Usage: node index.js spam [steamID] [pastebin ID] [spam interval]");
-            process.exit(1);
-         }
+         spammer.handleCommand(process.argv);
          break;
       case "autoreply":
-         if (process.argv[3] !== undefined) {
-            autoReply.setAutoReplyMsg(process.argv[3]);
-            autoReply.startAutoReply();
-         } else {
-            console.log("Usage: node index.js autoreply [message with underscore separators]");
-            process.exit(1);
-         }
+         autoReply.handleCommand(process.argv);
          break;
       default:
          console.log("Usage: node index.js [arg 1] [arg 2] ... [arg n]\n");
